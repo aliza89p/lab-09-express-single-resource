b7279ccc0bec55434a61e34988a07cf3f937c47a
Aliza Pilisuk <apilisuk@gmail.com>
2016-07-26 10:08:06 -0700

    added body parser and debugger packages, added error in userconstructor, started routing

--- lab-aliza/route/Router.js
+++ lab-aliza/route/Router.js
@@ -1,7 +1,21 @@
 'use strict';
 
 const Router = require('express').Router;
-const AppError = require('./model/AppError');
+const appError = require('../model/apperror');
+const UserConstructor = require('../model/userconstructor');
 let router = Router();
+var bodyParser = require('body-parser');
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
+router.post('/user', bodyParser, appError, (req, res) => {
+  res.status(200).json({id: req.body.id, name: req.body.name, creationDate: req.body.creationDate});
+});
+
+router.get('/user/:id/:name/:date', appError, (req, res) => {
+  res.status(200).json({id: req.params.id, name: req.params.name, creationDate: req.params.creationDate});
+});
+
+router.use('/api', router);
 
 module.exports = router;
