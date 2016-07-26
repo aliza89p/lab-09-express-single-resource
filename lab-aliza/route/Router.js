9aa2e4102e2f09941e428f1e18973f9a7df6986d
Aliza Pilisuk <apilisuk@gmail.com>
2016-07-26 11:25:38 -0700

    removed code from router that was bringing in apperror and userconstructor but breaking the server. Not finished, will resubmit

--- lab-aliza/route/Router.js
+++ lab-aliza/route/Router.js
@@ -1,18 +1,10 @@
 'use strict';
 
 const Router = require('express').Router;
-const appError = require('../model/apperror');
-const UserConstructor = require('../model/userconstructor');
 let router = Router();
 var bodyParser = require('body-parser');
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
-router.use(appError());
-
-let user = new UserConstructor();
-user.id = this.user.id;
-user.name = this.user.name;
-user.creationDate = this.user.creationDate;
 
 router.get('/user/:id', (req, res) => {
   res.status(200).json({id: req.params.id});
