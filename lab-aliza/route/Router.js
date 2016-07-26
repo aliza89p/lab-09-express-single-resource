d6a782f2bfd09e6e4365dea73eb3b2b96f957811
Aliza Pilisuk <apilisuk@gmail.com>
2016-07-26 14:33:58 -0700

    messing with router

--- lab-aliza/route/Router.js
+++ lab-aliza/route/Router.js
@@ -1,16 +1,21 @@
 'use strict';
 
 const Router = require('express').Router;
+const debug = require('debug');
+const serverLog = debug('serverlog');
+const appError = require('../model/apperror');
+const UserConstructor = require('../model/userconstructor');
 let router = Router();
 var bodyParser = require('body-parser');
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: true }));
+let jsonParser = bodyParser.json();
+router.use(jsonParser);
 
 router.get('/user/:id', (req, res) => {
   res.status(200).json({id: req.params.id});
 });
 
-router.post('/user', (req, res) => {
+router.post('/user', jsonParser, (req, res) => {
+  if(!req.body) return res.sendStatus(400);
   res.status(200).json({name: req.body.name});
 });
 
