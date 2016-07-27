d5be7c1b5277b80eefc5467ad921fcd4cd1b44b4
Aliza Pilisuk <apilisuk@gmail.com>
2016-07-27 12:02:26 -0700

    fixed error handling and added testing for get post and put

--- lab-aliza/test/test.js
+++ lab-aliza/test/test.js
@@ -7,7 +7,7 @@ const expect = chai.expect;
 const request = chai.request;
 const server = require('../server');
 
-describe('Http POST testing', () => {
+describe('POST testing', () => {
   it('Test valid POST request', (done) => {
     request(server)
       .post('/api/user')
@@ -36,20 +36,18 @@ describe('Http POST testing', () => {
   });
 });
 
-describe('Http GET testing', () => {
+describe('GET testing', () => {
+  let users = {};
   before((done) => {
     request(server)
       .post('/api/user')
       .send({
         name: 'aliza'
       })
-      .end(function(err, res) {
-        expect(err).to.eql(null);
-        expect(res).to.have.status(200);
-        expect(res.body).to.include.keys('id', 'name', 'creationDate');
-        expect(res.body.name).to.eql('aliza');
-        done();
-      });
+    .end(function(err, res){
+      users.id = res.body.id;
+      done();
+    });
   });
   it('test GET 404', (done) => {
     request(server)
@@ -57,6 +55,53 @@ describe('Http GET testing', () => {
     .end(function(err, res) {
       expect(res).to.have.status(404);
       expect(res.body).to.eql('404 not found');
+      done();
+    });
+  });
+  it('GET /user/<id>', (done) => {
+    request(server)
+    .get('/api/user/' + users.id)
+    .end(function(err, res) {
+      expect(err).to.eql(null);
+      expect(res).to.have.status(200);
+      expect(res.body.name).to.eql('aliza');
+      done();
+    });
+  });
+});
+
+describe('PUT testing', () => {
+  let users = {};
+  before((done) => {
+    request(server)
+      .post('/api/user')
+      .send({
+        name: 'aliza'
+      })
+    .end(function(err, res){
+      users.id = res.body.id;
+      done();
+    });
+  });
+  it('test PUT /user/<id> name=tyler', (done) => {
+    request(server)
+    .put('/api/user/' + users.id)
+    .send({
+      name: 'tyler'
+    })
+    .end(function(err, res) {
+      expect(err).to.eql(null);
+      expect(res).to.have.status(200);
+      expect(res.body.name).to.eql('tyler');
+      done();
+    });
+  });
+  it('PUT /user/1234', (done) => {
+    request(server)
+    .put('/api/user/1234')
+    .end(function(err, res) {
+      expect(res).to.have.status(400);
+      expect(res.body).to.eql('400 bad request');
       done();
     });
   });
