69b9642807bff2689ab39ca4a225ebe19563f860
Aliza Pilisuk <apilisuk@gmail.com>
2016-07-27 12:54:04 -0700

    fixing travis issue

--- lab-aliza/index.js
+++ lab-aliza/index.js
@@ -1,1 +1,1 @@
-module.exports = require('./route/router');
+module.exports = require('./route/Router');
--- lab-aliza/server.js
+++ lab-aliza/server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 let app = express();
-let router = require('./route/router');
+let router = require('./route/Router');
 
 app.use('/api', router);
 
