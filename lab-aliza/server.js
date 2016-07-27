4b1ee498ac977a6188d1587ddc679123b73149ab
Aliza Pilisuk <apilisuk@gmail.com>
2016-07-27 11:03:58 -0700

    added some testing

--- lab-aliza/server.js
+++ lab-aliza/server.js
@@ -7,3 +7,5 @@ let router = require('./route/router');
 app.use('/api', router);
 
 app.listen(3000, () => console.log('server up at 3000'));
+
+module.exports = app;
--- lab-aliza/test/test.js
+++ lab-aliza/test/test.js
@@ -3,15 +3,61 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
-
 const expect = chai.expect;
 const request = chai.request;
+const server = require('../server');
 
-require('../server');
+describe('Http POST testing', () => {
+  it('Test valid POST request', (done) => {
+    request(server)
+      .post('/api/user')
+      .send({
+        name: 'aliza'
+      })
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.body).to.include.keys('id', 'name', 'creationDate');
+        expect(res.body.name).to.eql('aliza');
+        done();
+      });
+  });
+  it('test POST error', (done) => {
+    request(server)
+    .post('/api/user')
+    .send({
+      blah: 'blah'
+    })
+    .end(function(err, res) {
+      expect(res).to.have.status(400);
+      expect(res.body).to.eql('400 bad request');
+      done();
+    });
+  });
+});
 
-describe('test', () => {
-  it('test', (done) => {
-    expect('test').to.eql('test');
-    done();
+describe('Http GET testing', () => {
+  before((done) => {
+    request(server)
+      .post('/api/user')
+      .send({
+        name: 'aliza'
+      })
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.body).to.include.keys('id', 'name', 'creationDate');
+        expect(res.body.name).to.eql('aliza');
+        done();
+      });
+  });
+  it('test GET 404', (done) => {
+    request(server)
+    .get('/api/user/123')
+    .end(function(err, res) {
+      expect(res).to.have.status(404);
+      expect(res.body).to.eql('404 not found');
+      done();
+    });
   });
 });
