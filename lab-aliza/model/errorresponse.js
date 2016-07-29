4cf4d087ef03682731e204fa70c570bbe74c073d
Aliza Pilisuk <apilisuk@gmail.com>
2016-07-28 19:58:47 -0700

    resubmitting with errorresponse and morgan

--- lab-aliza/model/errorresponse.js
+++ lab-aliza/model/errorresponse.js
@@ -6,7 +6,7 @@ const errorResponse = function() {
   return (err, req, res, next) => {
     if (Error.hasError(err)){ res.status(err.statusCode).send(err.resMessage);
     }
-    res.status(500).send('internal server problem');
+    res.status(500).send('500 internal server error');
     next();
   };
 };
