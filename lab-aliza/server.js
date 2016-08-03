1f8a6df0bc9aae15d367f6f0032f472b1664c145
Aliza Pilisuk <apilisuk@gmail.com>
2016-08-02 23:24:22 -0700

    added app.use for new error handeling

--- lab-aliza/server.js
+++ lab-aliza/server.js
@@ -2,15 +2,20 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const errorResponse = require('./model/errorresponse');
+const AppError = require('./model/AppError');
 let app = express();
 let router = require('./route/Router');
 
+app.use(errorResponse());
 
-app.use(morgan('combined'));
-
-
+app.use(morgan('dev'));
 
 app.use('/api', router);
+
+app.use((req, res) => {
+  res.sendError(AppError.error404('not found'));
+});
 
 app.listen(3000, () => console.log('server up at 3000'));
 
